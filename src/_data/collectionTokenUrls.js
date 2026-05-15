386fbc31e492b3154097c416158336313ae6d823
Keith Baron <k.baron@snhu.edu>
2026-05-14 20:11:56 -0400

    Centralize collection and group URL helpers

--- src/_data/collectionTokenUrls.js
+++ src/_data/collectionTokenUrls.js
@@ -1,10 +1,11 @@
 const collectionTokenPages = require("./collectionTokenPages");
+const urlHelpers = require("./urlHelpers");
 
 const urls = {};
 
 for (const page of collectionTokenPages) {
   const token = page.token || {};
-  const url = `/collection/${page.collectionArea.slug}/${page.pageId}/`;
+  const url = urlHelpers.collectionTokenUrl(page.collectionArea.slug, page.pageId);
   const keys = [token.id, token.displayId];
 
   for (const key of keys) {
